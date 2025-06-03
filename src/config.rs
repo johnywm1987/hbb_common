a43e1ef147cd928ce6327de01340ef52b1f9fdb2
johnywm1987 <yimweiming123@gmail.com>
2025-06-04 01:52:41 +0800

    change ip

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["168.231.78.72"];
+pub const RS_PUB_KEY: &str = "sy+hydVxaFoyD7uMrjNNiwfoAXuM1EP3auf+GWDGyDA=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
